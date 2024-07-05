060881b7242a5df15434f54769a6694978fcf330
MUSTAFA <msallat5@gmail.com>
2024-07-05 19:51:28 +0300

    Arrow keys

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('up').addEventListener('click', function() {
+    alert('Up button clicked');
+});
+
+document.getElementById('down').addEventListener('click', function() {
+    alert('Down button clicked');
+});
+
+document.getElementById('left').addEventListener('click', function() {
+    alert('Left button clicked');
+});
+
+document.getElementById('right').addEventListener('click', function() {
+    alert('Right button clicked');
+});
